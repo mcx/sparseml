2ab8dba7195fdfdf6e2e607c6d7bcd1d91e6b703
Tuan Nguyen <tuan@neuralmagic.com>
2024-02-09 08:29:56 -0500

    Updated script

--- my_scripts/prune_0.owl.sh
+++ my_scripts/prune_0.owl.sh
@@ -12,14 +12,22 @@ MODEL_DIR=$HOME/models/llama2/cnn_dailymail
 #SRC_MODEL=$MODEL_DIR/llama-recipes/dense_finetuned/Llama-2-7b-hf@lr8e-5@B16@GrAcc4@W0.1@ep1@GPUs8@WD0.0@ID$SRC_ID
 
 # 50% sparse finetuned model
-SRC_ID=3641
-SRC_MODEL=$MODEL_DIR/llama-recipes/sparse_finetuned/sparse_ft@SRC5043@lr8e-5@WD0.0@B8@GrAcc8@W0.1@ep1@GPUs7@ID$SRC_ID/hf
+#SRC_ID=3641
+#SRC_MODEL=$MODEL_DIR/llama-recipes/sparse_finetuned/sparse_ft@SRC5043@lr8e-5@WD0.0@B8@GrAcc8@W0.1@ep1@GPUs7@ID$SRC_ID/hf
+
+#################################
+# 50% sparse upstream model from Cerebras
+SRC_ID=cerebras50
+SRC_MODEL_DIR=/home/tuan/models/llama2/cerebras
+SRC_MODEL_NAME=llama2-7b@$SRC_ID
+SRC_MODEL=$SRC_MODEL_DIR/$SRC_MODEL_NAME
+
 
 DST_MODEL_DIR=$MODEL_DIR/owl
 
 M=5
 
-for SP in 70 80 85
+for SP in 60 70 80 85
 do
 for LMBDA in 0.02
 do
